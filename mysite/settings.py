868a4fed339472fbe7965b5e63ac057c49bb679e
kelvinhovinho <kelvinadamba@hotmail.com>
2025-11-27 20:51:14 +0300

    preparing my project for hosting

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*%bj!npcptrtot@(4i^czj$p^0qy1ijnpb7-(v!+qro=$9169f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
